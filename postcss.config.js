baa6b604b1057c5d8724569836e7d777b8f3592f
unknown <brankliz7@gmail.comgit config --global user.email brankliz7@gmail.com>
2024-12-27 12:10:32 +0300

    som 1.0

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+
+  
+  // After (CommonJS syntax)
+  module.exports = {
+    plugins: [
+      require('autoprefixer'),
+      require('postcss-preset-env'),
+      // Add other PostCSS plugins if needed
+    ],
+  };
+  
